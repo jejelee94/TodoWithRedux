54a1a650960328150ba22944c4adcc54f5d69ea3
jaekyung lee <jaekyunglee@jaekyungui-MacBookPro.local>
2021-10-08 15:05:09 +0900

    deleteTodo

--- TodoList/src/store.js
+++ TodoList/src/store.js
@@ -12,16 +12,16 @@ const addToDo = (text) => {
 const deleteToDo = (id) => {
 	return {
 		type: DELETE,
-		id,
+		id: parseInt(id),
 	};
 };
 
 const reducer = (state = [], action) => {
 	switch (action.type) {
 		case ADD:
-			return [{ type: action.text, id: Date.now() }, ...state];
+			return [{ text: action.text, id: Date.now() }, ...state];
 		case DELETE:
-			return state.filter((toDo) => toDo !== action.id);
+			return state.filter((toDo) => toDo.id !== action.id);
 		default:
 			return state;
 	}
